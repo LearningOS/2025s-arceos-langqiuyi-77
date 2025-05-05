1bc23b3aed74b07b4c1f6309ce59dae521bd6e72
qiuyi Lang <786659776@qq.com>
2025-05-05 12:11:47 +1000

    课后习题4 handle_page_fault

--- arceos/tour/m_1_0/src/syscall.rs
+++ arceos/tour/m_1_0/src/syscall.rs
@@ -1,8 +1,13 @@
 #![allow(dead_code)]
 
 use axhal::arch::TrapFrame;
-use axhal::trap::{register_trap_handler, SYSCALL};
+use axhal::trap::{register_trap_handler, SYSCALL, PAGE_FAULT};
+use axhal::paging::MappingFlags;
 use axerrno::LinuxError;
+use axhal::mem::VirtAddr;
+use axtask::{TaskExtMut, TaskExtRef};
+
+use crate::task::TaskExt;
 
 const SYS_EXIT: usize = 93;
 
@@ -20,4 +25,27 @@ fn handle_syscall(tf: &TrapFrame, syscall_num: usize) -> isize {
         }
     };
     ret
+}
+
+#[register_trap_handler(PAGE_FAULT)]
+fn handle_page_fault(
+    vaddr: VirtAddr,
+    access_flags: MappingFlags,
+    _is_user: bool,
+) -> bool {
+    let task = axtask::current();                  // ✅ 保存临时值
+    let task_ref = task.as_task_ref();             // ✅ 现在引用是安全的
+    let task_inner = task_ref.inner();             // ✅ OK
+    let ext = task_inner.task_ext();                    // ✅ OK
+    let mut aspace = ext.aspace.lock();            // ✅ OK
+
+    // 分配物理页帧
+    if aspace.handle_page_fault(vaddr, access_flags) {
+        return true;
+    }
+
+    panic!(
+        "Unhandled page fault @ {:#x} with access {:?}",
+        vaddr, access_flags
+    );
 }
